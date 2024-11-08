ce495bf75f2ff43e4a11e274aa62497560c166fa
Basab Dattaray <dattaraybasab0@gmail.com>
2024-11-07 20:36:46 -0800

    Updated producer module

---
+++ producer/go.mod
@@ -0,0 +1,4 @@
+module github.com/dattaray-basab/gologger/producer
+
+
+go 1.22.1
---
+++ producer/main.go
@@ -0,0 +1,17 @@
+// main.go
+
+package gologger
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("WARN - %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("ERROR - %v", message)
+}
